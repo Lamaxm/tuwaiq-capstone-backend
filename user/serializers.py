55cb2ff58a323a917144e8f7f3956ef77fba1532
NOURAH <nourahsaadan@gmail.com>
2022-07-25 16:56:00 +0300

    Added the user app

--- user/serializers.py
+++ user/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
-from .models import employee ,fav
+from django.contrib.auth.models import User
+from .models import Profile
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
 
 
-class favSerializer(serializers.ModelSerializer):
-    """For Serializing Comment"""
-    class Meta :
-        model = fav
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
         fields = '__all__'
 
--- user/views.py
+++ user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, authentication_classes,permission_classes
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+from rest_framework_simplejwt.tokens import AccessToken,Token
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserRegisterSerializer
+
+
+
+@api_view(['POST'])
+def login_user(request : Request):
+
+    if 'username' in request.data and 'password' in request.data:
+        user = authenticate(request, username=request.data['username'], password=request.data['password'])
+        if user is not None:
+            #create the token , then give the token to the user in the response
+            token = AccessToken.for_user(user)
+            responseData = {
+                "msg" : "Your token is ready",
+                "token" : str(token)
+            }
+            return Response(responseData)
+
+
+    return Response({"msg" : "please provide your username & password"}, status=status.HTTP_401_UNAUTHORIZED)
+
