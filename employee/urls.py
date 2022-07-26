84f5b73e73a764b4c04e57374aa09d536e2e10c4
NOURAH <nourahsaadan@gmail.com>
2022-07-26 11:20:17 +0300

    updated some file

--- employee/urls.py
+++ employee/urls.py
@@ -1,20 +1,20 @@
 from django.urls import path
 from . import views
 
-app_name = "employee"
+app_name = "emp"
 
 urlpatterns = [
     # CRUD Employee
-    path("emp/add_emp", views.add_emp, name="add_emp"),
-    path("emp/get_emp", views.all_employee, name="get_emp"),
-    path("emp/get_comp_emp", views.com_employees, name="get_comp_emp"),
-    path("edit_emp", views.edit_emp, name="edit_emp"),
-    path("delete_emp", views.delete_emp, name="delete_emp"),
+    path("emp/addemp", views.add_emp, name="add_emp"),
+    path("emp/allemps", views.get_emp, name="get_emp"),
+    path("emp/myemps", views.get_comp_emp, name="get_comp_emp"),
+    path("emp/editemp", views.edit_emp, name="edit_emp"),
+    path("emp/deleteemp", views.delete_emp, name="delete_emp"),
     # ---
-    path("add_req/<emp_id>", views.add_req, name="add_req"),
-    path("reqEmp", views.req_Emp, name="req_Emp"),
-    path("delete_req", views.delete_req, name="delete_req"),
-    path("add_fav", views.add_fav, name="add_fav"),
-    path("reqEmp", views.reqEmp, name="reqEmp"),
-    path("delete_req", views.delete_req, name="delete_req"),
+#     path("add_req/<emp_id>", views.add_req, name="add_req"),
+#     path("reqEmp", views.req_Emp, name="req_Emp"),
+#     path("delete_req", views.delete_req, name="delete_req"),
+#     path("add_fav", views.add_fav, name="add_fav"),
+#     path("reqEmp", views.reqEmp, name="reqEmp"),
+#     path("delete_req", views.delete_req, name="delete_req"),
 ]
