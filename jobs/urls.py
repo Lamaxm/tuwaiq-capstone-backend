b226ccf1642dee8d6399cd0a28cf94c767aab021
lama <l4m4100@gmail.com>
2022-07-26 10:43:03 +0300

    edit wmployee and jobs

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "user"
+app_name = "jobs"
 
 urlpatterns = [
-    path("/add_job", views.add_job, name="add_job"),
-    path("/all_jobs", views.all_jobs, name="add_job"),
-    path("/delete_job", views.delete_job, name="add_job"),
-    path("/edit_job", views.edit_job, name="add_job"),
+    path("add_job", views.add_job, name="add_job"),
+    path("all_jobs", views.all_jobs, name="add_job"),
+    path("delete_job", views.delete_job, name="add_job"),
+    path("edit_job", views.edit_job, name="add_job"),
 ]
