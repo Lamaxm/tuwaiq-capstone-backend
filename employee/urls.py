90f146433c43e720909e67d3985cfd04ff565204
NOURAH <nourahsaadan@gmail.com>
2022-07-26 15:18:55 +0300

    update urls

--- employee/urls.py
+++ employee/urls.py
@@ -5,11 +5,11 @@ app_name = "emp"
 
 urlpatterns = [
     # CRUD Employee
-    path("addemp/", views.add_emp, name="add_emp"),
-    path("emp/allemps", views.get_emp, name="get_emp"),
-    path("emp/myemps", views.get_comp_emp, name="get_comp_emp"),
-    path("emp/editemp", views.edit_emp, name="edit_emp"),
-    path("emp/deleteemp", views.delete_emp, name="delete_emp"),
+    path("add_emp/", views.add_emp, name="add_emp"),
+    path("get_emp/", views.get_emp, name="get_emp"),
+    path("my_emp/", views.get_comp_emp, name="get_comp_emp"),
+    path("edit_emp/", views.edit_emp, name="edit_emp"),
+    path("delete_emp/", views.delete_emp, name="delete_emp"),
     # ---
 #     path("add_req/<emp_id>", views.add_req, name="add_req"),
 #     path("reqEmp", views.req_Emp, name="req_Emp"),
