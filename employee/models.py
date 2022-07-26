da0307166049a653d565f6d6bd0d0181f7c6a079
NOURAH <nourahsaadan@gmail.com>
2022-07-26 11:16:06 +0300

    updated some files

--- employee/models.py
+++ employee/models.py
@@ -8,7 +8,7 @@ class Employee(models.Model):
     linkedin = models.URLField()
     state = models.BooleanField(default=False)
     add_date = models.DateField(auto_now_add=True , blank=True)
-    company = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- employee/urls.py
+++ employee/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
 
-app_name = "wmployee"
+app_name = "employee"
 
 urlpatterns = [
-    path("add_emp", views.add_emp, name="add_emp"),
-    path("all_employee", views.all_employee, name="all_employee"),
-    path("com_employees", views.com_employees, name="com_employees"),
+    # CRUD Employee
+    path("emp/add_emp", views.add_emp, name="add_emp"),
+    path("emp/get_emp", views.all_employee, name="get_emp"),
+    path("emp/get_comp_emp", views.com_employees, name="get_comp_emp"),
     path("edit_emp", views.edit_emp, name="edit_emp"),
     path("delete_emp", views.delete_emp, name="delete_emp"),
+    # ---
     path("add_req/<emp_id>", views.add_req, name="add_req"),
     path("reqEmp", views.req_Emp, name="req_Emp"),
     path("delete_req", views.delete_req, name="delete_req"),
